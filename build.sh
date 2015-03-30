c500741fcb45b2fe02d3b5e5fc91368553eb5abb
Gil Meir <gilmeir@mellanox.com>
2015-03-30 15:02:04 +0300

    Use correct fuel-plugin-builder package file in build script
    * added better error handling
    
    Change-Id: I3e6d0bba3d9389fa16ae1e5618f788d6d87f8fcd
    Signed-off-by: Gil Meir <gilmeir@mellanox.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
 #!/bin/bash -ex
 
+function fail_on_error() {
+  RC=$1
+  MSG=$2
+  if [ $RC -ne 0 ]; then
+    echo $MSG >&2
+    exit 1
+  fi
+}
+
 # Verify distrobution
-if [ ! -f /etc/redhat-release ] && [ ! -f /etc/centos-release ];then
-  echo "Mellanox plugin build script supports only Redhat / CentOS distro."
-  exit 1
-fi
+[ -f /etc/redhat-release ] || [ -f /etc/centos-release ] ||
+fail_on_error $? "Mellanox plugin build script supports only Redhat / CentOS distro."
 
 readonly TOP_DIR=`dirname $0`
 readonly TMP_DIR='/tmp'
 readonly PLUGIN_BUILDER_DIR="$TMP_DIR/fuel-plugins/fuel_plugin_builder"
-
+readonly PLUGIN_BUILDER_PACKAGE_DIR="$PLUGIN_BUILDER_DIR/dist"
 pushd $TOP_DIR > /dev/null
 
 # Clean if needed
@@ -19,25 +26,30 @@ fi
 if [ -d ".build" ]; then
   rm -rf .build
 fi
-pip freeze | grep -q fuel-plugin-builder && pip uninstall fuel-plugin-builder -y
+
+pip freeze | grep -q fuel-plugin-builder &&
+( pip uninstall fuel-plugin-builder -y || fail_on_error $? "Failed uninstalling fuel-plugin-builder" )
 rm -rf *.rpm
 
 # Install prerequisites
-yum install rpm rpm-build dpkg-devel createrepo tar git python-pip -y
+yum install rpm rpm-build dpkg-devel createrepo tar git python-pip -y ||
+fail_on_error $? "Failed installing some required packages"
 
 # Download Fuel plugin builder
-git clone https://github.com/stackforge/fuel-plugins.git $TMP_DIR/fuel-plugins
+git clone https://github.com/stackforge/fuel-plugins.git $TMP_DIR/fuel-plugins ||
+fail_on_error $? "Failed cloning fuel-plugin-builder git repository"
 
 # Append bootstrap post install to the spec
 cat specs/post.spec >> $PLUGIN_BUILDER_DIR/fuel_plugin_builder/templates/build/plugin_rpm.spec.mako
 
 # Install plugin builder
 pushd $PLUGIN_BUILDER_DIR > /dev/null
-python setup.py sdist
-pip install dist/fuel-plugin-builder-2.0.0.dev.tar.gz
+rm -f $PLUGIN_BUILDER_PACKAGE_DIR/*
+python setup.py sdist || fail_on_error $? "Failed building fuel-plugin-builder from source"
+pip install $(ls -d $PLUGIN_BUILDER_PACKAGE_DIR/*) || fail_on_error $? "Failed installing fuel-plugin-builder"
 popd > /dev/null
 
 # Create Mellanox Plugin
-fuel-plugin-builder --build .
+fuel-plugin-builder --build . || fail_on_error $? "Building mellanox plugin failed"
 
 popd > /dev/null
