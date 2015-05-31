efaa3d26c6de92bb37c7bb585c9e10281f4b8420
Aviram Bar-Haim <aviramb@mellanox.com>
2015-05-31 17:34:32 +0300

    Add SRIOV snapshot patch for hostdev devices
    Change-Id: I583e4ab3af9711347f4431282318865dfa4ecf29

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/params.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/params.pp
@@ -11,12 +11,14 @@ class mellanox_openstack::params {
       $agent_service            = 'neutron-mlnx-agent'
       $compute_service_name     = 'openstack-nova-compute'
       $openvswitch_mgmt_service = 'openvswitch'
+      $libvirt_driver_file       = '/usr/lib/python2.6/site-packages/nova/virt/libvirt/driver.py'
     }
     'Debian': {
       $neutron_mlnx_packages    = ['neutron-plugin-mlnx','neutron-plugin-mlnx-agent']
       $agent_service            = 'neutron-plugin-mlnx-agent'
       $compute_service_name     = 'nova-compute'
       $openvswitch_mgmt_service = 'openvswitch-switch'
+      $libvirt_driver_file       = '/usr/lib/python2.7/dist-packages/nova/virt/libvirt/driver.py'
     }
   }
 
