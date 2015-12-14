9afd6167839865aaf87420b78ecf7bafee5298d8
Rawan Herzallah <rherzallah@asaltech.com>
2015-12-14 10:01:01 +0000

    Fuel 8.0 POC
    New configurations for fuel mellanox plugin to use sriovnicswitch
    mechanism driver for ethernet networks, it also gives the user the
    option to enable vxlan offloading to imporove performance and reduce
    CPU overhead.
    
    Change-Id: I480d4efb31fde126a51594bdbf48307a4e63ea74

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/compute_sriov.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/compute_sriov.pp
@@ -1,11 +1,28 @@
 class mellanox_openstack::compute_sriov (
   $physnet,
   $physifc,
+  $mlnx_driver,
+  $firewall_driver,
+  $exclude_vf,
 ) {
 
   include nova::params
   $libvirt_service_name = 'libvirtd'
   $libvirt_package_name = $nova::params::libvirt_package_name
+
+  $pci_passthrough_addresses = $mellanox_openstack::get_pci_passthrough_addresses($exclude_vf, $physnet)
+
+  if ( $mlnx_driver == 'mlx4_en' ){
+    # configure pci_passthrough_whitelist nova compute
+    nova_config { 'DEFAULT/pci_passthrough_whitelist':
+        value => check_array_of_hash($pci_passthrough_addresses);
+    }
+
+    # update [securitygroup] section in neutron
+    neutron_plugin_ml2 { 'securitygroup/firewall_driver':    
+       value => $firewall_driver;
+    }
+  }
 
   class { 'mellanox_openstack::eswitchd' :
       physnet => $physnet,
