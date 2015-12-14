956ebd1f551b790e7245074089f3e60620781e56
Rawan Herzallah <rherzallah@asaltech.com>
2015-12-14 10:01:01 +0000

    Fuel 8.0 POC
    New configurations for fuel mellanox plugin to use sriovnicswitch mechanism driver for ethernet networks,
    it also gives the user the option to enable vxlan offloading to imporove performance and reduce CPU overhead.
    
    Change-Id: I480d4efb31fde126a51594bdbf48307a4e63ea74

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/controller_sriov.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/controller_sriov.pp
@@ -3,7 +3,10 @@ class mellanox_openstack::controller_sriov (
   $eswitch_apply_profile_patch,
   $mechanism_drivers,
   $mlnx_driver,
-  $mlnx_sriov
+  $mlnx_sriov,
+  $pci_vendor_devices,
+  $agent_required
+
 ) {
 
   include neutron::params
@@ -13,15 +16,35 @@ class mellanox_openstack::controller_sriov (
   include mellanox_openstack::params
   $package              = $::mellanox_openstack::params::neutron_mlnx_packages_controller
 
+  ## add nova conf but how ???
+  nova_config { 'DEFAULT/scheduler_default_filters': 
+    value => join($scheduler_default_filters,', PciPassthroughFilter')
+  }
+
   package { $package :
         ensure => installed,
   }
 
   neutron_plugin_ml2 {
-    'eswitch/vnic_type':            value => $eswitch_vnic_type;
-    'eswitch/apply_profile_patch':  value => $eswitch_apply_profile_patch;
-    'ml2/mechanism_drivers':        value => "mlnx,${mechanism_drivers}";
+    'eswitch/vnic_type':                      value => $eswitch_vnic_type;
+    'eswitch/apply_profile_patch':            value => $eswitch_apply_profile_patch;
+
+    #check if infiniband
+    if ( $mlnx_driver == 'eth_ipoib' ){
+      'ml2/mechanism_drivers':                  value => "mlnx,${mechanism_drivers}";
+    } else if ( $mlnx_driver == 'mlx4_en' ){
+      'ml2/mechanism_drivers':                  value => "sriovnicswitch,${mechanism_drivers}";
+    }
+
+    # add to ml2_sriov section
+    'ml2_sriov/supported_pci_vendor_devs':    value => $pci_vendor_devices;
+
+    # agent required
+    'ml2_sriov/agent_required':               value => $agent_required;
   }
+
+  ## retart nuetron server with conf files, how ??
+
 
   service { $server_service :
     ensure => running
