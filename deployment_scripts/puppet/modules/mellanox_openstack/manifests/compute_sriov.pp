c03921affabefe64d21a5ee454e7565e5eada099
Rawan Herzallah <rherzallah@asaltech.com>
2015-12-14 10:01:01 +0000

    Add support for sriovnicswitch and VXLAN offload
    New configurations for fuel mellanox plugin to use sriovnicswitch
    mechanism driver for ethernet networks, it also gives the user the
    option to enable vxlan offloading to imporove performance and reduce
    CPU overhead.
    
    Change-Id: I480d4efb31fde126a51594bdbf48307a4e63ea74

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/compute_sriov.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/compute_sriov.pp
@@ -1,35 +1,70 @@
 class mellanox_openstack::compute_sriov (
   $physnet,
   $physifc,
+  $mlnx_driver,
+  $firewall_driver,
+  $exclude_vf,
 ) {
 
   include nova::params
+  include mellanox_openstack::params
+
+  $sriov_agent_service  = $::mellanox_openstack::params::sriov_agent_service_name
+  $sriov_agent_package  = $::mellanox_openstack::params::sriov_agent_package_name
   $libvirt_service_name = 'libvirtd'
   $libvirt_package_name = $nova::params::libvirt_package_name
 
-  class { 'mellanox_openstack::eswitchd' :
-      physnet => $physnet,
-      physifc => $physifc,
+  $path_to_generate_pci_script = generate ("/bin/bash", "-c", 'echo /etc/fuel/plugins/mellanox-plugin-*.0/generate_pci_passthrough_whitelist.py | tr -d \'\n \' ')
+  $pci_passthrough_addresses = generate ("/usr/bin/python", $path_to_generate_pci_script, $exclude_vf, $physnet)
+
+  if ( $mlnx_driver == 'mlx4_en' ){
+    # configure pci_passthrough_whitelist nova compute
+    nova_config { 'DEFAULT/pci_passthrough_whitelist':
+      value => check_array_of_hash("${pci_passthrough_addresses}"),
+    }
+
+    # update [securitygroup] section in neutron
+    neutron_plugin_ml2 { 'securitygroup/firewall_driver':
+      value => $firewall_driver,
+    }
+
+    package { $sriov_agent_package:
+      ensure => installed,
+    }
+
+    service { $sriov_agent_service:
+      ensure     => running,
+      enable     => true,
+      hasstatus  => true,
+      hasrestart => true,
+    }
+
+    Package[$sriov_agent_package] ->
+    Service[$sriov_agent_service]
+
+  } elsif ( $mlnx_driver == 'eth_ipoib' ){
+
+    class { 'mellanox_openstack::eswitchd' :
+        physnet => $physnet,
+        physifc => $physifc,
+    }
+
+    class { 'mellanox_openstack::agent' :
+        physnet => $physnet,
+        physifc => $physifc,
+    }
+
+    package { $libvirt_package_name :
+        ensure => installed
+    }
+
+    service { $libvirt_service_name :
+        ensure => running
+    }
+
+    Package[$libvirt_package_name] ->
+    Service[$libvirt_service_name] ->
+    Class['mellanox_openstack::eswitchd'] ~>
+    Class['mellanox_openstack::agent']
   }
-
-  class { 'mellanox_openstack::agent' :
-      physnet => $physnet,
-      physifc => $physifc,
-  }
-
-  class { 'mellanox_openstack::snapshot_patch' : }
-
-  package { $libvirt_package_name :
-      ensure => installed
-  }
-
-  service { $libvirt_service_name :
-      ensure => running
-  }
-
-  Package[$libvirt_package_name] ->
-  Service[$libvirt_service_name] ->
-  Class['mellanox_openstack::eswitchd'] ~>
-  Class['mellanox_openstack::agent']
-
 }
