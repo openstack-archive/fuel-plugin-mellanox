9f484c26c74dc6be90b99a9ed93c5fa65592cc60
Rawan Herzallah <rherzallah@asaltech.com>
2015-12-14 10:01:01 +0000

    Fuel 8.0 POC
    New configurations for fuel mellanox plugin to use sriovnicswitch
    mechanism driver for ethernet networks, it also gives the user the
    option to enable vxlan offloading to imporove performance and reduce
    CPU overhead.
    
    Change-Id: I480d4efb31fde126a51594bdbf48307a4e63ea74

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -3,8 +3,15 @@ $quantum_settings = hiera('quantum_settings')
 $mlnx = hiera('mellanox-plugin')
 
 if ($mlnx['sriov']) {
+  $firewall_driver = 'neutron.agent.firewall.NoopFirewallDriver'
+  $pci_passthrough_addresses = generate ("/bin/bash", "-c", 'python /etc/fuel/plugins/mellanox-plugin-*.0/generate_pci_passthrough_whitelist.py "0"')
+  #$physnet = $quantum_settings['predefined_networks']['net04']['L2']['physnet']
+  #$pci_passthrough_list = '{ "address":"${pci_passthrough_addresses},"physical_network":"${physnet}"}'
   class { 'mellanox_openstack::compute_sriov' :
     physnet => $quantum_settings['predefined_networks']['net04']['L2']['physnet'],
     physifc => $mlnx['physical_port'],
+    pci_passthrough => $pci_passthrough_addresses,
+    firewall_driver => $firewall_driver,
   }
 }
+
--- deployment_scripts/sriov.sh
+++ deployment_scripts/sriov.sh
@@ -35,7 +35,9 @@ function get_port_type() {
 }
 
 function get_num_probe_vfs () {
-  if [ $ISER == true ] && [ $DRIVER == 'mlx4_en' ]; then
+  if [ $SRIOV == true ] && [ $DRIVER == 'mlx4_en' ]; then
+    probe_vfs=`calculate_total_vfs`
+  elif [ $ISER == true ] && [ $DRIVER == 'mlx4_en' ]; then
     probe_vfs=1
   else
     probe_vfs=0
@@ -79,22 +81,36 @@ function reduce_mac_caching_timeout () {
   sysctl_conf set 'net.ipv4.route.gc_timeout' "$timeout"
 }
 
+function is_vxlan_offloading_required () {
+  [ $VXLAN_OFFLOADING == true ]
+  return $?
+}
+
 function set_modprobe_file () {
   PROBE_VFS=`get_num_probe_vfs`
   MLX4_CORE_FILE="/etc/modprobe.d/mlx4_core.conf"
   PORT_TYPE=`get_port_type`
   MLX4_CORE_STR="options mlx4_core
                  enable_64b_cqe_eqe=0
-                 log_num_mgm_entry_size=-1
-                 port_type_array=${PORT_TYPE},${PORT_TYPE}"
+                 debug_level=1"
+
   TOTAL_VFS=$1
-  if [[ $TOTAL_VFS -gt 0 ]]; then
-    MLX4_CORE_STR="${MLX4_CORE_STR} num_vfs=${TOTAL_VFS}"
-    if [[ $PROBE_VFS -gt 0 ]]; then
-      MLX4_CORE_STR="${MLX4_CORE_STR} probe_vf=${PROBE_VFS}"
+  if is_vxlan_offloading_required; then
+    MLX4_CORE_STR="${MLX4_CORE_STR} log_num_mgm_entry_size=-1"
+  fi
+  if is_sriov_required || is_vxlan_offloading_required ; then
+    MLX4_CORE_STR="${MLX4_CORE_STR} port_type_array=${PORT_TYPE},${PORT_TYPE}"
+
+    if [[ $TOTAL_VFS -gt 0 ]]; then
+      MLX4_CORE_STR="${MLX4_CORE_STR} num_vfs=${TOTAL_VFS}"
+      if [[ $PROBE_VFS -gt 0 ]]; then
+        MLX4_CORE_STR="${MLX4_CORE_STR} probe_vf=${TOTAL_VFS}"
+      fi
     fi
   fi
+
   echo ${MLX4_CORE_STR} > ${MLX4_CORE_FILE}
+
 }
 
 function set_kernel_params () {
@@ -164,6 +180,9 @@ function configure_sriov () {
     fi
     logger_print info "Configuring ${total_vfs} virtual functions
                        (only even number is currently supported)"
+
+    probe_vfs=`get_num_probe_vfs`
+    port_type=`get_port_type`
     set_modprobe_file $total_vfs &&
     set_kernel_params &&
     burn_vfs_in_fw $total_vfs
@@ -203,7 +222,9 @@ function validate_sriov () {
 
   # fallback only if kernel param exists and amount of vfs is not as expcted
   logger_print error "Failed , trying to fallback to ${FALLBACK_NUM_VFS}"
-  set_modprobe_file $FALLBACK_NUM_VFS
+  probe_vfs=`get_num_probe_vfs`
+  port_type=`get_port_type`
+  set_modprobe_file $FALLBACK_NUM_VFS 
   service openibd restart &> /dev/null
   current_num_vfs=`lspci | grep -i mellanox | grep -i virtual | wc -l`
   if [ $current_num_vfs -eq $FALLBACK_NUM_VFS ]; then
