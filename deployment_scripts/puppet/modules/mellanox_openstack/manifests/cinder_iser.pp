362b635b7c4f3d0c5ef57c1f617f34961ce12f77
Aviram Bar-Haim <aviramb@mellanox.com>
2016-02-11 15:22:19 +0200

    Move iSER configurations to use iscsi_protocol
    Starting from Liberty release LVM iSER volume driver is deprecated and
    iSER configurations are being done using iscsi_protocol parameter.
    Moreover, in order to use iSER transport protocol, a physical
    endpoint is required.
    
    Change-Id: I03046e229ccc221a4a391ad3cf7d03721aa2574d

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/cinder_iser.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/cinder_iser.pp
@@ -3,26 +3,25 @@ class mellanox_openstack::cinder_iser (
 ) {
   include cinder::params
 
-  class { 'mellanox_openstack::cinder::cinder_conf' :
-    iser_ip_address => $iser_ip_address,
-  } ~>
-  service { $cinder::params::volume_service :
-    ensure => running
-  }
-}
-
-class mellanox_openstack::cinder::cinder_conf (
-  $iser_ip_address,
-) {
-  include cinder::params
-  include mellanox_openstack::params
-
   cinder_config { 'DEFAULT/volume_driver' :
-    value => 'cinder.volume.drivers.lvm.LVMISERDriver'
+    value => 'cinder.volume.drivers.lvm.LVMVolumeDriver'
   }
-  cinder_config { 'DEFAULT/iser_ip_address' :
-    value => $iser_ip_address
+  cinder_config { 'DEFAULT/iscsi_protocol' :
+    value => 'iser'
+  }
+  cinder_config { 'DEFAULT/iscsi_ip_address' :
+    value => "$iser_ip_address"
+  }
+  exec { 'flush_br_storage' :
+    command => "ip addr flush dev br-storage",
+    onlyif  => "ip a | grep -q br-storage",
+    path    => ['/bin', '/sbin']
+  }
+  service { $cinder::params::volume_service :
+    ensure    => running,
+    subscribe => [Cinder_config['DEFAULT/iscsi_protocol'],
+                  Cinder_config['DEFAULT/volume_driver'],
+                  Exec['flush_br_storage'],
+                  Cinder_config['DEFAULT/iscsi_ip_address']]
   }
 }
-
-
