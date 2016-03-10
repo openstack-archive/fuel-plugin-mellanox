59b2b36d7fadec31ae51866285c11def233c507e
Rawan Herzallah <rherzallah@asaltech.com>
2016-03-10 17:42:41 +0000

    Fix QoS Configurations
    Added compute neutron sriov agent related configurations.
    
    Change-Id: I64ce8be3d90c90cd43c93d6a6b2214cf864796a1

--- deployment_scripts/puppet/manifests/compute.pp
+++ deployment_scripts/puppet/manifests/compute.pp
@@ -3,6 +3,7 @@ $quantum_settings = hiera('quantum_settings')
 $mlnx = hiera('mellanox-plugin')
 $firewall_driver = 'neutron.agent.firewall.NoopFirewallDriver'
 $private_net = $quantum_settings['default_private_net']
+$roles = hiera('roles')
 
 if ( $mlnx_driver == 'mlx4_en' ){
   $exclude_vf = '0'
@@ -17,5 +18,13 @@ if ($mlnx['sriov']) {
     mlnx_driver         => $mlnx['driver'],
     firewall_driver     => $firewall_driver,
     exclude_vf          => $exclude_vf,
+  }
+}
+
+# Configure QoS for ETH
+if ( $mlnx['driver'] == 'mlx4_en' and $mlnx['mlnx_qos'] ) {
+  class { 'mellanox_openstack::configure_qos' :
+    mlnx_sriov => $mlnx['sriov'],
+    roles      => $roles
   }
 }
