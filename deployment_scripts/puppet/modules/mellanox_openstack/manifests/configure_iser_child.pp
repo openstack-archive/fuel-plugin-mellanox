23c4cf34949fa3b666f82d9b673c3de3259373a2
Aviram Bar-Haim <aviramb@mellanox.com>
2016-03-18 18:47:41 +0200

    Fix IB iSER after reboot and untagged iSER support
    Change-Id: Ie2195e9b6a52ebae7f4876b09d94d158b022bb91

--- deployment_scripts/puppet/modules/mellanox_openstack/manifests/configure_iser_child.pp
+++ deployment_scripts/puppet/modules/mellanox_openstack/manifests/configure_iser_child.pp
@@ -3,6 +3,9 @@ class mellanox_openstack::configure_iser_child (
   $iser_child,
   $iser_ipaddr
 ) {
+
+  $ifcfg_child="/etc/network/interfaces.d/ifcfg-$iser_child"
+
   l23_stored_config { $iser_parent:
     ipaddr   => none,
     method   => static
@@ -14,6 +17,11 @@ class mellanox_openstack::configure_iser_child (
   l23_stored_config { $iser_child:
     ipaddr   => $iser_ipaddr,
     method   => static
+  } ~>
+  exec { 'add-post-up-parent-clean':
+    command     => "/bin/echo 'post-up ip addr flush dev $iser_parent' >> $ifcfg_child",
+    onlyif      => "/usr/bin/test -f $ifcfg_child && ! /bin/grep -q post-up $ifcfg_child",
+    refreshonly => true,
   } ~>
   exec { 'refresh-iser-child':
     command     => "/sbin/ifdown $iser_child ; /sbin/ifup $iser_child",
