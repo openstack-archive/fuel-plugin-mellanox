fd0d04ae9f24fa7c1c356cee0a1e7c2098ce1163
Rawan Herzallah <rherzallah@asaltech.com>
2016-06-13 10:31:41 +0000

    Fixed post install script output message with MAX num vfs parameter
    Enhanced the output after adding max_num_vfs parameter to
    create_mellanox_bootstrap script
    
    Change-Id: I4496fc7e40c810355ba244eefc3762e87aede819

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,19 @@
+
+# Copyright 2016 Mellanox Technologies, Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 source /var/www/nailgun/plugins/mellanox-plugin*/scripts/common
 
 if [ -d $FUEL_BOOTSTRAP_DIR ]; then
@@ -11,9 +27,11 @@ if [ -d $FUEL_BOOTSTRAP_DIR ]; then
       /sbin/
 
   # Print post install message
-  echo "  `tput bold`In order to create Bootstrap discovery image for detecting Mellanox Infiniband HW:"
-  echo "    1. Please build a new bootstrap ('create_mellanox_bootstrap [--link_type] [-h, --help] can be used')."
+  echo "  `tput bold`In order to create Bootstrap discovery image for detecting"\
+       "Mellanox Infiniband HW:"
+  echo "    1. Please build a new bootstrap ('create_mellanox_bootstrap"\ 
+       "[ -h | --link_type (eth | ib | current) [ --max_num_vfs <max_number_of_vfs> ]]')"
   echo "    2. Please reboot your old bootstrap nodes"
-  echo "       ('reboot_bootstrap_nodes [-e environment_id] [-a] [-h]' can be used).`tput sgr0`"
+  echo "       ('reboot_bootstrap_nodes [ -h | -a | -e <environment_id> ]' can be used).`tput sgr0`"
 
 fi
